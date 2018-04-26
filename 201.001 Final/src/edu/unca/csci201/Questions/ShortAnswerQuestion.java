ab46d78406da0ec93029eb423e9919cdaf50ea9c
MasterSquinch <fallenstars@icloud.com>
2018-04-26 16:25:17 -0400

    Update ShortAnswerQuestion.java

--- 201.001 Final/src/edu/unca/csci201/Questions/ShortAnswerQuestion.java
+++ 201.001 Final/src/edu/unca/csci201/Questions/ShortAnswerQuestion.java
@@ -4,23 +4,39 @@ import edu.unca.csci201.Question;
 
 public class ShortAnswerQuestion implements Question{
 	boolean isAdded = false;
+	String keyphrase = "";
+	String correctAnswer = "";
+	String text = "";
+	int threshold;
 
+	
+	public ShortAnserQuestion(String text, String keyphrase, int threshold) {
+		this.text = text;
+		this.keyphrase = keyphrase;
+		this.threshshold = threshold;
+	}
+	
 	@Override
 	public String getTheQuestionText() {
-		// TODO Auto-generated method stub
-		return null;
+		return text;
 	}
 
 	@Override
 	public boolean isCorrectAnswer(String answer) {
-		// TODO Auto-generated method stub
+		int i = 0;
+		Pattern p = Pattern.compile( this.keyphrase );
+		Matcher m = p.matcher( answer );
+		while (m.find()) {
+   		 i++;
+		}
+		if ( this.threshold <= i )
+			return true;
 		return false;
 	}
 
 	@Override
 	public String getCorrectAnswer() {
-		// TODO Auto-generated method stub
-		return null;
+		return correctAnswer;
 	}
 
 }
